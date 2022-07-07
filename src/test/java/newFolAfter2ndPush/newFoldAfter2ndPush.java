74526d056870de7425506ac8494b0c6f16603438
Shiva Kumar M <shivakumar12054@gmail.com>
2022-07-07 16:25:13 +0530

    added feature 2time

---
+++ src/test/java/newFolAfter2ndPush/newFoldAfter2ndPush.java
@@ -0,0 +1,7 @@
+package newFolAfter2ndPush;
+
+public class newFoldAfter2ndPush {
+    public static void main(String[] args) {
+        System.out.println("sdsdsdsdsdsdsd");
+    }
+}
---
+++ src/test/java/newFolAfter2ndPush/newFoldAfter2ndPush2.java
@@ -0,0 +1,7 @@
+package newFolAfter2ndPush;
+
+public class newFoldAfter2ndPush2 {
+    public static void main(String[] args) {
+        System.out.println("ssdsdsdsdsdsdsdsd");
+    }
+}
