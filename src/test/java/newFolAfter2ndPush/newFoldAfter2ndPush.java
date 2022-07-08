a255d4207bde7193da8d6dafaaa2bb9b01be7619
kumar12054 <108915252+kumar12054@users.noreply.github.com>
2022-07-08 10:04:54 +0530

    added comments

--- src/test/java/newFolAfter2ndPush/newFoldAfter2ndPush.java
+++ src/test/java/newFolAfter2ndPush/newFoldAfter2ndPush.java
@@ -3,5 +3,6 @@ package newFolAfter2ndPush;
 public class newFoldAfter2ndPush {
     public static void main(String[] args) {
         System.out.println("sdsdsdsdsdsdsd");
+        //added some comments
     }
 }
